5047711d68db61e409025da3cc3306cea37653e3
hveiled <andrei.vad.ivanov@gmail.com>
2022-02-09 16:25:36 +0500

    Setters/Getters are removed instead of Lombok @Data

--- src/main/java/com/example/taskStorage/model/Project.java
+++ src/main/java/com/example/taskStorage/model/Project.java
@@ -1,15 +1,19 @@
 package com.example.taskStorage.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
-
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import java.sql.Date;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Project entity model
+ */
+@Data
 @Entity
 @Table(name = "project")
 public class Project {
@@ -24,11 +28,12 @@ public class Project {
 	String projectName;
 
 	@Column(name = "project_start_date")
-	@NotBlank(message = "Project start date should not be blank")
-	Date projectStartDate;
+	@Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
+	String projectStartDate;
 
 	@Column(name = "project_completion_date")
-	Date projectCompletionDate;
+	@Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
+	String projectCompletionDate;
 
 	@Column(name = "current_status")
 	@Enumerated(EnumType.STRING)
@@ -38,67 +43,11 @@ public class Project {
 	@Min(value = 0, message = "Project priority should not be negative or zero")
 	int priority;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getProjectName() {
-		return projectName;
-	}
-
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
-
-	public Date getProjectStartDate() {
-		return projectStartDate;
-	}
-
-	public void setProjectStartDate(Date projectStartDate) {
-		this.projectStartDate = projectStartDate;
-	}
-
-	public Date getProjectCompletionDate() {
-		return projectCompletionDate;
-	}
-
-	public void setProjectCompletionDate(Date projectCompletionDate) {
-		this.projectCompletionDate = projectCompletionDate;
-	}
-
-	public ProjectCurrentStatus getCurrentStatus() {
-		return currentStatus;
-	}
-
-	public void setCurrentStatus(ProjectCurrentStatus currentStatus) {
-		this.currentStatus = currentStatus;
-	}
-
-	public int getPriority() {
-		return priority;
-	}
-
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
-
 	@OneToMany(
 			cascade = CascadeType.ALL, fetch = FetchType.EAGER,
 			mappedBy = "project", orphanRemoval = true)
 	@JsonManagedReference
 	private List<Task> tasks = new ArrayList<>();
-
-	public List<Task> getTasks() {
-		return tasks;
-	}
-
-	public void setTasks(List<Task> taskList) {
-		this.tasks = taskList;
-	}
 
 	@Override
 	public String toString() {
--- src/main/java/com/example/taskStorage/model/Task.java
+++ src/main/java/com/example/taskStorage/model/Task.java
@@ -1,6 +1,7 @@
 package com.example.taskStorage.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -8,6 +9,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
+@Data
 @Entity
 @Table(name = "task")
 public class Task {
@@ -30,73 +32,15 @@ public class Task {
 	@Enumerated(EnumType.STRING)
 	private TaskCurrentStatus status;
 
-//	@Column(name = "project_id")
-//	private Long projectId;
-
-//	public Long getProjectId() {
-//		return projectId;
-//	}
-//
-//	public void setProjectId(Long projectId) {
-//		this.projectId = projectId;
-//	}
-
 	@Min(value = 0, message = "Priority should not be negative or zero")
 	@Column(name = "priority")
 	private int priority;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTaskName() {
-		return taskName;
-	}
-
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
-
-	public String getTaskDescription() {
-		return taskDescription;
-	}
-
-	public void setTaskDescription(String taskDescription) {
-		this.taskDescription = taskDescription;
-	}
-
-	public TaskCurrentStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(TaskCurrentStatus status) {
-		this.status = status;
-	}
-
-	public int getPriority() {
-		return priority;
-	}
-
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
-
-	public Project getProject() {
-		return project;
-	}
 
 	@ManyToOne
 	@JoinColumn(name = "project_id")
 	@JsonBackReference
 	private Project project;
 
-	public void setProject(Project project) {
-		this.project = project;
-	}
 
 	@Override
 	public boolean equals(Object o) {
@@ -104,11 +48,6 @@ public class Task {
 		if (o == null || getClass() != o.getClass()) return false;
 		Task task = (Task) o;
 		return priority == task.priority && id.equals(task.id) && Objects.equals(taskName, task.taskName) && Objects.equals(taskDescription, task.taskDescription) && status == task.status && Objects.equals(project, task.project);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, taskName, taskDescription, status, priority, project);
 	}
 
 	@Override
